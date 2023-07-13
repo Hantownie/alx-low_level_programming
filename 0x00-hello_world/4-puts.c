82c79fdcaa9fd8552d41a0083fb32af580553978
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-13 15:46:12 -0700

    whitespaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/**
+ * print_with_puts - prints without printf
+ * nb: followed by new line
+ */
 
 int main(void)
 {
-	char pro[] = "Programming is like building a multilingual puzzle";
+	char pro[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(pro);
 	return (0);
