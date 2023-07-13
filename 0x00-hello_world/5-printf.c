28ddc9080d0d9872c5b9b7563f7d8c489ba5cd93
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-13 16:15:35 -0700

    printff

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Main - print with prinf
+ * main - print with prinf
  * Return: zero
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
