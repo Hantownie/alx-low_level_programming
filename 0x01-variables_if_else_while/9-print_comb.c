ed4d1ef78ca48b6ae4c7e3f6fe2f7ac2bf219b0b
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-14 16:58:31 -0700

    commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry level
+ * Description - this prints single digit number
+ * Return: 0 success
+*/
+int main(void)
+{
+	int di;
+
+	for (di = 0; di <= 9; di++)
+	{
+		putchar(di + '0');
+		if (di < 9)
+		{
+			putchar(",");
+			putchar(" ");
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
