fdb56341da0161b2d2afd2af9eaba8bb5e30248d
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-14 15:26:59 -0700

    exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry level
+ * Description - this prints alphabets in lower case except two
+ * Return: 0 success
+*/
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		if (al != 'q' && al != 'e')
+		{
+			putchar(al);
+		}
+	}
+	putchar('\n');
+	return (0);
