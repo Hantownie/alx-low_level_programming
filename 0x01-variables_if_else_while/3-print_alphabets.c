10e1d3252c82995aa837c0232d1bc4359d6ef745
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-14 15:10:17 -0700

    double

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry level
+ * Description - this prints alphabets in lower case
+ * Return: 0 success
+*/
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		putchar(al);
+	}
+	for (al = 'A'; al <= 'Z'; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n')
+		return (0);
+}
