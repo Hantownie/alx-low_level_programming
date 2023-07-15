7a025246963e4bc4b4cca0c4ddd7dc2521f6b464
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-14 19:17:02 -0700

    finished

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry level
+ * Description - this prints triple digit number combinations
+ * Return: 0 success
+*/
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a; b <= 99; b++)
+		{
+			int f1 = a / 10;
+			int f2 = a % 10;
+			int f3 = b / 10;
+			int f4 = b % 10;
+
+			putchar(f1 + '0');
+			putchar(f2 + '0');
+			putchar(' ');
+			putchar(f3 + '0');
+			putchar(f4 + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
