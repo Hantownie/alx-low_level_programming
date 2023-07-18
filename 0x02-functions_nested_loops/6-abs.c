afedd03eac34e431af39a26c3621489f907b18ff
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-18 15:53:31 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - this prints the absolute value of a number
+ * @n: the character
+ * Return: it returns positive or negative number
+*/
+int _abs(int n);
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		_putchar('-');
+		return (n);
+	}
+}
