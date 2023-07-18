0f71ad0689a4b151202171a80f7805bab12d6e4b
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-18 07:47:18 -0700

    lowercasealpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry level
+ * Description - this prints lowercase alphabet
+ * Return: 0 success
+*/
+int main(void)
+{
+	int al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		_putchar(al);
+	}
+	_putchar('\n')
+	return (0);
+}
