8ee9bdc05745e6a77758bb7252febaf5750b2bc2
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-18 16:29:12 -0700

    ldigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - this  prints the last digit of the number provided
+ * @b: the number
+ * Return: it returns the last digit of a number
+*/
+int print_last_digit(int b)
+{
+	int a = b % 10;
+
+	return (a);
+}
