767a38975abdc34188a1b12264f46c12671a9964
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-18 17:55:17 -0700

    nine

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * times_table - prints one to nine times table
+ * Return: it returns the multiploes of zero to nine
+*/
+void times_table(void)
+{
+	int i, t;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (t = 0; t <= 9; t++)
+		{
+			int tbl = i * t;
+
+			_putchar((tbl / 10) + '0');
+			_putchar((tbl % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
