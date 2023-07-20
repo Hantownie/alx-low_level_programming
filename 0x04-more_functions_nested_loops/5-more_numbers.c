4fe11f987e0f5140141c90d44cf5550633bf1bcb
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-20 12:20:13 -0700

    tenloopss

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,18 @@
 */
 void more_numbers(void)
 {
-	int a, b;
+	int b;
 
-	for (a = 0; a < 10; a++)
+	for (int a = 0; a < 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b);
+			if (b > 9)
+			{
+				_putchar((b / 10) + '0');
+			}
+			_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
