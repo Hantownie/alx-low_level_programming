e49b599b885549d008b087e474e78be42892a18f
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-20 11:46:20 -0700

    tenloop

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - this prints numbers 0 to 14 ten times
+ * Return: it returns zero to fourteen ten times
+*/
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			_putchar(b + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
