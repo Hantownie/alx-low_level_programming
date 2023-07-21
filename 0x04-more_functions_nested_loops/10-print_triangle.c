af43a7254b461c5a59d83887841095503a4be940
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-20 18:10:03 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,3 +5,26 @@
  * Return: void
 */
 void print_triangle(int size)
+{
+	int l, sh, sp;
+
+	if (size > 0)
+	{
+		for (l = 1; l <= size; l++)
+		{
+			for (sp = size - l; sp >= 1; sp--)
+			{
+				_putchar(' ');
+			}
+			for (sh = 1; sh <= l; sh++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
