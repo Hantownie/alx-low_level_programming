95c2f4ae2529d015ee91641bb53548c061f613b3
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-24 14:27:09 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - this prints every other line
+ * @str: pointer parameter
+ * Return: string
+*/
+void puts2(char *str)
+{
+	int inc = 0;
+
+	while (str[inc] != '\0')
+	{
+		_putchar(str[inc]);
+		inc += 2;
+	}
+}
