6ee3682a46b42155287c21f061287792a4085d3d
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-24 16:00:57 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - this prints half of a line
+ * @str: pointer parameter
+ * Return: string
+*/
+void puts_half(char *str)
+{
+	int a, b;
+	int inc = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		inc++;
+	}
+	b = (inc / 2);
+	if ((inc % 2) == 1)
+	{
+		b = ((inc + 1) / 2));
+	}
+	for (a = b; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
