38abcc0d20fb4b3bb900416d3032c024e7fa0bf0
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-07-24 14:04:51 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverses string
+ * @s: pointer parameter
+ * Return: string
+*/
+void rev_string(char *s)
+{
+	char temp = s{0};
+	int count = 0;
+	int a;
+
+	while (s[count] != '\0')
+		count++;
+	for (a = 0; a < count; a++)
+	{
+		count--;
+		temp = s[a];
+		s[a] = s[count];
+		s[count] = temp;
+	}
+}
