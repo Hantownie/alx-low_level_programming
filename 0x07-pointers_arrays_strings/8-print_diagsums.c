3d6cdb06562927f915a5096c63b513fbfca8a876
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-08-01 14:58:01 -0700

    diagsum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - prints the sum of a diagonal
+ * @a: array
+ * @size: size of matrix
+ * Return: sums
+*/
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int s1 = 0;
+	int s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		a += size;
+	}
+	a -= size;
+	for (i = 0; i < size; i++)
+	{
+		s2 += a[i];
+		a -= size;
+	}
+	printf("%d, %d\n", s1, s2);
+}
