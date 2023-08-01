20394889c96e7ace5a2973c6602ce0b66408172f
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-08-01 10:02:25 -0700

    strfixed

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,15 +7,21 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
+	int i;
+
 	if (*needle == 0)
 	{
 		return (haystack);
 	}
 	while (*haystack)
 	{
-		if (*haystack == *needle)
+		if (haystack[i] == needle[i])
 		{
-			return (haystack);
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
 	}
