db82d2310629f6d4ea83ee01017ba38a21410e9a
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-08-01 07:11:50 -0700

    meme

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory
+ * @b: constant byte
+ * @n: number to fill
+ * Return: string
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
