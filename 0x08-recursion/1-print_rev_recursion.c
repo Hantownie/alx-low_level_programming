577f6cd86c32c6f9f1fe27ed345837fb74b35ee4
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-08-02 12:58:18 -0700

    revrecursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - puts a stringin reverse with recursion
+ * @s: character
+ * Return: void
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
