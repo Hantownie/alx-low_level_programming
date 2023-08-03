0d1e1e658e3767554a52d289f1b82cc298f8ca7e
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-08-02 17:07:12 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * primechck - checks if number is prime
+ * @num: number
+ * @pr: factor
+ * Return: 0 and 1
+*/
+int primechck(int num, int pr)
+{
+	if (pr < num)
+	{
+		if (num % pr == 0)
+		{
+			return (0);
+		}
+		else
+			return (primechck(num, pr + 1));
+	}
+	else
+		return (0);
+}
+
+/**
+ * is_prime_number - identifies prime number
+ * @n: number
+ * Return: 0 and 1
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else
+		return (primechck(n, 2));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrtchck(int v, int sq);
+int is_prime_number(int n);
+int primechck(int num,int pr);
 
 #endif
