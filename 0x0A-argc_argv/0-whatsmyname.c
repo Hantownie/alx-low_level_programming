ab9b4cbc59204ae327a9a2f38366750ff376cd77
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-08-04 11:53:54 -0700

    whatsmy

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry level
+ * @argc: argument count
+ * @argv: argument vector
+ * Description - this prints name followed by newline
+ * Return: 0 success
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
