58055ff022e61e94a0e49bb667a8ed8d1f85d3df
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-08-04 13:22:27 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry level
+ * @argc: argument count
+ * @argv: argument vector
+ * Description - this prints multiplication
+ * Return: 0 success
+*/
+int main(int argc, char *argv[])
+{
+	int mul = 0;
+
+	if (argc <= 1)
+	{
+		printf("Error");
+	}
+	else
+	{
+		mul += atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	return (0);
+}
