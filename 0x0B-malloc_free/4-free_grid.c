d979cf0041b4f77b829d71e06e0794c05cc49283
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-08-09 07:07:03 -0700

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - frees space allocated by function
+ * @grid: array
+ * @height: rows
+ * Return: null if fail
+*/
+void free_grid(int **grid, int height)
+{
+	int i, j;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
