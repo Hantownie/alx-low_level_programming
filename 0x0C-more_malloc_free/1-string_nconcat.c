0b240b68095393c3a519cee8bd6d0c8c27a217eb
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-08-10 11:16:28 -0700

    rem

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < l2)
 		ar = malloc(sizeof(char) * (l1 + n + 1));
 	else
-		ar = malloc(sizeof(char) * (li + l2 + 1));
+		ar = malloc(sizeof(char) * (l1 + l2 + 1));
 	if (!ar)
 		return (NULL);
 	while (i < l1)
