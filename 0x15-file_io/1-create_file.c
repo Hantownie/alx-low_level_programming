d027ec5d0d41a46ad19e259a7e6be26c31404942
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-09-04 08:33:30 -0700

    bugs

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,8 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		i = 0;
-		while (text_content[i] != '\0')
+		for (i = 0; text_content[i];)
 			i++;
 	}
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
