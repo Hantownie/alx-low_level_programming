2f79e1401d12bd8051e80a6a8e9fb7065d0f7caf
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-09-04 08:52:59 -0700

    fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,15 +15,20 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (op == -1)
+		return (-1);
 	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i];)
 			i++;
+		temp = write(op, text_content, i);
+		if (temp == -1)
+		{
+			close(op);
+			return (-1);
+		}
 	}
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	temp = write(op, text_content, i);
-	if (op == -1 || temp == -1)
-		return (-1);
 	close(op);
 	return (1);
 }
