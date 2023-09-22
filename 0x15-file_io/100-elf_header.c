772391e5add8a08dddd5a8782a8b9a4a149f882d
Ezeudegbe Anthony <tonyeze13@gmail.com>
2023-09-22 14:58:24 -0700

    tryed

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -48,7 +48,7 @@ void print_magic(unsigned char *e_ident)
 {
 	int index;
 
-	printf(" Magic:   ");
+	printf("Magic:   ");
 	for (index = 0; index < EI_NIDENT; index++)
 	{
 		printf("%02x", e_ident[index]);
@@ -64,7 +64,7 @@ void print_magic(unsigned char *e_ident)
  */
 void print_class(unsigned char *e_ident)
 {
-	printf(" Class:                             ");
+	printf("Class:                             ");
 	switch (e_ident[EI_CLASS])
 	{
 		case ELFCLASSNONE:
@@ -86,7 +86,7 @@ void print_class(unsigned char *e_ident)
  */
 void print_data(unsigned char *e_ident)
 {
-	printf(" Data:                              ");
+	printf("Data:                              ");
 	switch (e_ident[EI_DATA])
 	{
 		case ELFDATANONE:
@@ -108,7 +108,7 @@ void print_data(unsigned char *e_ident)
  */
 void print_version(unsigned char *e_ident)
 {
-	printf(" Version:                           %d", e_ident[EI_VERSION]);
+	printf("Version:                           %d", e_ident[EI_VERSION]);
 	switch (e_ident[EI_VERSION])
 	{
 		case EV_CURRENT:
@@ -126,7 +126,7 @@ void print_version(unsigned char *e_ident)
  */
 void print_osabi(unsigned char *e_ident)
 {
-	printf(" OS/ABI:                            ");
+	printf("OS/ABI:                            ");
 	switch (e_ident[EI_OSABI])
 	{
 		case ELFOSABI_NONE:
@@ -170,7 +170,7 @@ void print_osabi(unsigned char *e_ident)
  */
 void print_abi(unsigned char *e_ident)
 {
-	printf(" ABI Version:                       %d\n", e_ident[EI_ABIVERSION]);
+	printf("ABI Version:                       %d\n", e_ident[EI_ABIVERSION]);
 }
 
 /**
@@ -182,7 +182,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
-	printf(" Type:                              ");
+	printf("Type:                              ");
 	switch (e_type)
 	{
 		case ET_NONE:
@@ -212,7 +212,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
-	printf(" Entry point address:               ");
+	printf("Entry point address:               ");
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		e_entry = ((e_entry << 8) & 0xFF00FF00) |
